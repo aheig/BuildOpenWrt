455fc558d364f7c2ac4f90fc84b76bb493b5fe76
aheig <61814388+aheig@users.noreply.github.com>
2021-10-16 14:08:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,11 +1,6 @@
 #There is nothing.
 #You can view others files in this repository.
 #Good luck to you.
-# Disable DHCP server
-sed -i 's/start/ignore/' package/network/services/dnsmasq/files/dhcp.conf
-sed -i 's/100/1/' package/network/services/dnsmasq/files/dhcp.conf
-sed -i '28d' package/network/services/dnsmasq/files/dhcp.conf
-sed -i '28d' package/network/services/dnsmasq/files/dhcp.conf
 #
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
