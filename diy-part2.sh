6ae9d5af312d25bbb2af59ebd4d070194eca58d4
aheig <61814388+aheig@users.noreply.github.com>
2021-10-28 23:43:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,6 +2,10 @@
 #You can view others files in this repository.
 #Good luck to you.
 #
+# 修改内核5.14
+sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.14/g' target/linux/x86/Makefile
+sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=5.14/g' target/linux/x86/Makefile
+#
 # Modify default IP
 # sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 # Change SSH Prot
